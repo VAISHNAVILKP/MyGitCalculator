5ba0268318212803d06306bae6ae5dd776a58f0c
HP <HP@192.168.1.76>
2025-07-04 20:00:15 +0530

    Multiplication division functionality implemented

--- src/Interface/Calculator.java
+++ src/Interface/Calculator.java
@@ -3,5 +3,13 @@ package Interface;
 //public interface Calculator {
 	public interface Calculator {
 		
+		public int mul(int x, int y) {
+			return mul;
+		}
+		
+		public int div(int c, int d) {
+			return div;
+		}
+		
 	}
 
--- src/InterfaceImpl/CalulatorImpl.java
+++ src/InterfaceImpl/CalulatorImpl.java
@@ -3,4 +3,20 @@ package InterfaceImpl;
 import Interface.Calculator;
 public class CalulatorImpl implements Calculator{
 
+	@Override
+	public int mul(int x, int y) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int div(int c, int d) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+	
+	
+
 }
